373b3a66bb9a451f36e7a251b278eb01953a808b
Diversantlux <diversantlux@gmail.com>
2019-11-25 16:23:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,57 @@
 import {BehaviorSubject, combineLatest, fromEvent, of} from 'rxjs'
-import {
-    map,
-    pairwise,
-    switchMap,
-    takeUntil,
-    withLatestFrom,
-    startWith,
-    pluck,
-    tap,
-    skip,
-    filter,
-    flatMap
-} from 'rxjs/operators'
+import {filter, flatMap, map, pairwise, pluck, startWith, takeUntil, tap, withLatestFrom} from 'rxjs/operators'
 
-const canvas = document.querySelector('canvas')
-const range = document.getElementById('range')
-const color = document.getElementById('color')
-const clear = document.getElementById('clear')
-const gum = document.getElementById('gum')
-const line = document.getElementById('line')
+const canvas = document.querySelector('canvas');
+const range = document.getElementById('range');
+const color = document.getElementById('color');
+const clear = document.getElementById('clear');
+const gum = document.getElementById('gum');
+const line = document.getElementById('line');
 
-const ctx = canvas.getContext('2d')
-const rect = canvas.getBoundingClientRect()
-const scale = window.devicePixelRatio
+const ctx = canvas.getContext('2d');
+const rect = canvas.getBoundingClientRect();
+const scale = window.devicePixelRatio;
 
-canvas.width = rect.width * scale
-canvas.height = rect.height * scale
-ctx.scale(scale, scale)
+canvas.width = rect.width * scale;
+canvas.height = rect.height * scale;
+ctx.scale(scale, scale);
 
-const mouseMove$ = fromEvent(canvas, 'mousemove')
-const mouseDown$ = fromEvent(canvas, 'mousedown')
-const mouseUp$ = fromEvent(canvas, 'mouseup')
-const mouseOut$ = fromEvent(canvas, 'mouseout')
+const mouseMove$ = fromEvent(canvas, 'mousemove');
+const mouseDown$ = fromEvent(canvas, 'mousedown');
+const mouseUp$ = fromEvent(canvas, 'mouseup');
+const mouseOut$ = fromEvent(canvas, 'mouseout');
 
-const mouseClick$ = fromEvent(line, 'click')
-const mouseGum$ = fromEvent(gum, 'click')
+const lineBtnClick$ = fromEvent(line, 'click');
+const gumBtnClick$ = fromEvent(gum, 'click');
 
-const lineSubject =  new BehaviorSubject(false)
-mouseClick$.pipe(tap(() => {
-
+const lineSubject = new BehaviorSubject(false);
+lineBtnClick$.pipe(tap(() => {
 
     lineSubject.next(!lineSubject.getValue())
 
 })).subscribe();
 
-const lineSubjectGum =  new BehaviorSubject(false)
-mouseGum$.pipe(tap(() => {
+const lineSubjectGum = new BehaviorSubject(false);
+gumBtnClick$.pipe(tap(() => {
 
     lineSubjectGum.next(!lineSubjectGum.getValue())
 })).subscribe();
 
-// const swapBtn = combineLatest(
-//     lineSubject.asObservable().distinctUntilChanged())
-//     // lineSubjectGum.asObservable().distinctUntilChanged()])
-//     .subscribe()
 
+const buttonsInitialState = {gum: false, line: false, text: false};
+const buttonsStateSubject = new BehaviorSubject(buttonsInitialState);
+lineBtnClick$.pipe(tap(() => buttonsStateSubject.next({...buttonsInitialState, line: true}))).subscribe();
+gumBtnClick$.pipe(tap(() => buttonsStateSubject.next({...buttonsInitialState, gum: true}))).subscribe();
+buttonsStateSubject.pipe(tap(({gum: gumState, line: lineState}) => {
+
+    line.classList.remove('selected');
+    gum.classList.remove('selected');
+    if (lineState) {
+        line.classList.toggle('selected');
+    } else if (gumState) {
+        gum.classList.toggle('selected');
+    }
+})).subscribe();
 
 
 function createInputStream(node) {
@@ -64,78 +61,60 @@ function createInputStream(node) {
             startWith(node.value)
         )
 }
-
-const lineWidth$ = createInputStream(range)
-const strokeStyle$ = createInputStream(color)
-// const strokeLine$ = createInputStream(line)
-
-
-const lineState$ = lineSubject.asObservable();
-const lineStateGum$ = lineSubjectGum.asObservable();
+const lineSelected$ = buttonsStateSubject.pipe(pluck('line'), filter(Boolean));
+lineSelected$.pipe(tap(() => ctx.globalCompositeOperation = 'source-over')).subscribe();
+const lineWidth$ = createInputStream(range);
+const strokeStyle$ = createInputStream(color);
 
 
-
-lineState$
-    .pipe(skip(1))
-    .pipe(tap((state) => line.classList.toggle('selected')))
-    .subscribe();
-
-lineStateGum$
-    .pipe(skip(1))
-    .pipe(tap((state) => gum.classList.toggle('selected')))
-    .subscribe();
-
-
-
-lineState$
-    .pipe(filter(Boolean))
+lineSelected$
 
     .pipe(flatMap(() => {
 
         return mouseDown$
 
     })).pipe(
-    withLatestFrom(lineWidth$, strokeStyle$,   (_, lineWidth,  strokeStyle, ) => {
-        return {lineWidth,strokeStyle}
+    withLatestFrom(lineWidth$, strokeStyle$, (_, lineWidth, strokeStyle,) => {
+        return {lineWidth, strokeStyle}
     })).pipe(flatMap((options) => {
-        return combineLatest([mouseMove$.pipe(
-            map( e =>({
-                x: e.offsetX,
-                y: e.offsetY,
-                options
-            })))
+    return combineLatest([mouseMove$.pipe(
+        map(e => ({
+            x: e.offsetX,
+            y: e.offsetY,
+            options
+        })))
 
-           .pipe(
-               pairwise(),takeUntil(mouseUp$),takeUntil(mouseOut$)), of(options)
+        .pipe(
+            pairwise(), takeUntil(mouseUp$), takeUntil(mouseOut$)), of(options)
     ])
 
 }))
     .pipe(tap(([[from, to], {lineWidth, strokeStyle}]) => {
 
-    ctx.lineWidth = lineWidth
-    ctx.strokeStyle = strokeStyle
+        ctx.lineWidth = lineWidth;
+        ctx.strokeStyle = strokeStyle;
 
-    ctx.beginPath()
-    ctx.moveTo(from.x, from.y)
-    ctx.lineTo(to.x, to.y)
-    ctx.stroke()
+        ctx.beginPath();
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y);
+        ctx.stroke()
 
-})).subscribe()
+    })).subscribe();
 
 
 
-const strokeGum$ = fromEvent(gum, 'click')
+const strokeGum$ = gumBtnClick$
     .pipe(
         pluck('target', 'value'),
         tap(() => ctx.globalCompositeOperation = 'destination-out')
+    );
 
-    )
 
 const strokeClear$ = fromEvent(clear, 'click')
     .pipe(
         pluck('target', 'value'),
         tap(() => ctx.clearRect(0, 0, canvas.width, canvas.height))
-    )
+    );
 
 
 const steam$ = mouseDown$
@@ -143,18 +122,16 @@ const steam$ = mouseDown$
         withLatestFrom(strokeClear$, (_, strokeClear) => {
             return {strokeClear}
         })
-    )
+    );
 //
 //
 const gumim$ = mouseDown$
     .pipe(
         withLatestFrom(strokeGum$, (_, strokeGum) => {
             return {strokeGum}
-        }))
-gumim$.subscribe()
-steam$.subscribe()
-
-
+        }));
+gumim$.subscribe();
+steam$.subscribe();
 
 
 // function createInputStream(node) {
